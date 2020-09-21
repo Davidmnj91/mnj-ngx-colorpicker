c2cfc0f6da82ba83d0563ce7de64b8cd037da5a1
David Manjavacas <d.manjavacas@docu.com>
2020-09-21 09:52:01 +0200

    fix(test): main test fixed

--- projects/app/src/app/app.component.spec.ts
+++ projects/app/src/app/app.component.spec.ts
@@ -1,9 +1,33 @@
 import { TestBed } from '@angular/core/testing';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MnjColorpickerModule } from 'mnj-ngx-colorpicker';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        FlexLayoutModule,
+        MatToolbarModule,
+        MatCheckboxModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatInputModule,
+        MatRadioModule,
+        MatButtonModule,
+        MatCardModule,
+        MatMenuModule,
+        MnjColorpickerModule,
+      ],
       declarations: [AppComponent],
     }).compileComponents();
   });
@@ -12,18 +36,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('app app is running!');
   });
 });
